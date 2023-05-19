28c37dc865d3e0a81e69c9feeed6fbd996a8d150
cedric <ywang50@nd.edu>
2023-05-18 21:35:33 -0400

    docstring of NoFAS.py

--- NoFAS.py
+++ NoFAS.py
@@ -24,10 +24,10 @@ class FNN(nn.Module):
     def forward(self, x):
         """
         Args:
-            x: torch.Tensor [num_samples, input_size], assumed to be a batch.
+            x: torch.Tensor. [num_samples, input_size], assumed to be a batch.
 
         Returns:
-            torch.Tensor, assumed to be a batch.
+            torch.Tensor. Assumed to be a batch.
         """
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -76,6 +76,15 @@ class Surrogate:
 
     @limits.setter
     def limits(self, limits):
+        """
+        Set the limits of all inputs
+        Args:
+            limits: list or tuple. [[low_0, high_0], [low_1, high_1], ...]
+
+        Returns:
+            None
+
+        """
         limits = torch.Tensor(limits).tolist()
         if len(limits) != self.input_size:
             print("Error: Invalid input size for limit. Abort.")
@@ -94,6 +103,15 @@ class Surrogate:
 
     @pre_grid.setter
     def pre_grid(self, pre_grid):
+        """
+        Assign the pregrid of surrogate model. If nofas is not enabled, this serves as the whole training grid.
+        Args:
+            pre_grid: None or torch.Tensor, [data_num, feature_dim]. If None, will try to search for files npz containing this info.
+
+        Returns:
+            None
+
+        """
         if pre_grid is None:
             if path.exists(self.model_name + '.npz'):
                 container = np.load(self.model_name + '.npz')
@@ -113,6 +131,16 @@ class Surrogate:
             self.grid_record = self.__pre_grid.clone()
 
     def gen_grid(self, input_limits=None, gridnum=4, store=True):
+        """
+        Generate pre-grid.
+        Args:
+            input_limits: None or list[list]. If None, use self.limits. If list[list], rewrite self.limits and use it
+            gridnum: int. the number of grid points per dimension.
+            store: boolean. Store the pre-grid or not in self.pre_grid.
+
+        Returns:
+            torch.Tensor. [gridnum ** dim, dim]
+        """
         meshpoints = []
         if input_limits is not None:
             self.limits = input_limits
@@ -128,11 +156,21 @@ class Surrogate:
         return grid
 
     def surrogate_save(self):
+        """
+        Save surrogate model to [self.name].sur and [self.name].npz
+        Returns:
+            None
+        """
         torch.save(self.surrogate.state_dict(), self.model_name + '.sur')
         np.savez(self.model_name, limits=self.limits, pre_grid=self.pre_grid,
                  grid_record=self.grid_record)
 
     def surrogate_load(self):
+        """
+        Load surrogate model from [self.name].sur and [self.name].npz
+        Returns:
+            None
+        """
         self.surrogate.load_state_dict(torch.load(self.model_name + '.sur'))
         container = np.load(self.model_name + '.npz')
         for key in container:
@@ -143,6 +181,18 @@ class Surrogate:
                 print("Warning: [" + key + "] is not a surrogate variables.")
 
     def pre_train(self, max_iters, lr, lr_exp, record_interval, store=True, reg=False):
+        """
+        Train surrogate model with pre-grid. Optimizer is RMSprop; Exponential Scheduler enabled.
+        Args:
+            max_iters: int. The maximal number of iterations.
+            lr: double. Learning rate for RMSprop.
+            lr_exp: double. Decay factor for exponential scheduler.
+            record_interval: int. The number of iterations to print loss info
+            store: boolean. If true, self.surrogate_save() will be called.
+            reg: boolean. If true, L1 regularization will be used with parameter 0.0001
+        Returns:
+            None
+        """
         grid = (self.pre_grid - self.m) / self.sd
         out = (self.pre_out - self.tm) / self.tsd
         optimizer = torch.optim.RMSprop(self.surrogate.parameters(), lr=lr)
@@ -166,9 +216,25 @@ class Surrogate:
                     print('iter {}\t loss {}\t reg_loss {}'.format(i, loss, reg_loss))
                 else:
                     print('iter {}   loss {}'.format(i, loss))
-        if store: self.surrogate_save()
+        if store:
+            self.surrogate_save()
 
     def update(self, x, max_iters=10000, lr=0.01, lr_exp=0.999, record_interval=500, store=False, tol=1e-5, reg=False):
+        """
+        Model calibration for NoFAS. Optimizer is RMSprop, Scheduler is exponential.
+        Args:
+            x: torch.Tensor. Input feature that needs true model output.
+            max_iters: int. Maximal number of iteration. Default 10000.
+            lr: double. Learning rate for RMSprop. Default 0.01
+            lr_exp: double. Decay factor of exponential scheduler.
+            record_interval: int. The number of iterations to print loss information.
+            store: boolean. If ture, self.surrogate_save() will be called.
+            tol: double. Optimization will be terminated if loss < tol ** 2
+            reg: boolean. If ture, L1 regularizaiton will be used with parameter 0.1
+
+        Returns:
+            None
+        """
         self.grid_record = torch.cat((self.grid_record, x), dim=0)
         s = torch.std(x, dim=0)
         thresh = 0.1
@@ -208,12 +274,16 @@ class Surrogate:
                 print('Updating: {}\t loss {}'.format(i, loss), end='\r')
             if loss < tol ** 2: break
         print('                                                        ', end='\r')
-        if store: self.surrogate_save()
+        if store:
+            self.surrogate_save()
 
     def forward(self, x):
+        """
+        forward function of surrogate.
+        Args:
+            x: torch.Tensor. [data_num, feature_dim]
+
+        Returns:
+
+        """
         return self.surrogate((x - self.m) / self.sd) * self.tsd + self.tm
-
-
-if __name__ == '__main__':
-    S = Surrogate("Trivial", None, 3, 2, [[-7, 7], [-7, 7], [-7, 7]], 10, "Trivial_pregrid.txt")
-    print(S.pre_grid)
