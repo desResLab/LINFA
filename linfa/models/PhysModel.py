59678511845ffdb1ee85fc2a5d1216b7bab53b5b
jublslee <jubilee.dahee.lee@gmail.com>
2023-06-16 16:15:42 +0900

    added function for PhysModel

--- linfa/models/PhysModel.py
+++ linfa/models/PhysModel.py
@@ -2,4 +2,13 @@ import numpy as np
 import torch
 
 class Phys:
-    pass
+    def __init__(self):
+        # define parameter 
+        # inputs[0] = 1, [1] = 5, [2] = 60
+        self.defParam = torch.Tensor([[1.0, 5.0, 60.0]])
+
+    def genDataFile(self):
+        pass
+
+    def solve_t(self):
+        pass
