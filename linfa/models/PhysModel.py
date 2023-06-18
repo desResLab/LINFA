d00b9829bb1af44c4136702ea5ecd065377be42e
jublslee <jubilee.dahee.lee@gmail.com>
2023-06-18 19:21:47 +0900

    phys model changes

--- linfa/models/PhysModel.py
+++ linfa/models/PhysModel.py
@@ -10,5 +10,7 @@ class Phys:
     def genDataFile(self):
         pass
 
-    def solve_t(self):
-        pass
+    def solve_t(self, params):
+        z1, z2, z3 = torch.chunk(params, chunks=3, dim=1)
+        x = torch.cat((z1 ** 3 / 10, torch.exp(z2 / 3)), 1)
+        return torch.matmul(x, self.RM)
