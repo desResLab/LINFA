00529d391627ec47180b4293ff774f9e4b7c4b33
jublslee <jubilee.dahee.lee@gmail.com>
2023-06-19 15:02:15 +0900

    first genDataFile changes

--- linfa/models/PhysModel.py
+++ linfa/models/PhysModel.py
@@ -9,9 +9,16 @@ class Phys:
         self.RM = torch.Tensor([[1.0, 1.0],
                                 [1.0, -1.0]])
         self.gConst = 9.81
+        self.stdRatio = 0.05
+        self.data = None
 
-    def genDataFile(self):
-        pass
+    def genDataFile(self, dataSize = 50, dataFileName="source/data/data_phys.txt", store=True):
+        def_out = self.solve_t(self.defParam)[0]
+        print(def_out)
+        self.data = def_out + self.stdRatio * torch.abs(def_out) * torch.normal(0, 1, size=(dataSize, 3))
+        self.data = self.data.t().detach().numpy()
+        if store: np.savetxt(dataFileName, self.data)
+        return self.data
 
     def solve_t(self, params):
         z1, z2, z3 = torch.chunk(params, chunks=3, dim=1)
