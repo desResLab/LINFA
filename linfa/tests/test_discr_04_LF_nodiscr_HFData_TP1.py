20fcd8acbc8c4373316f724f8c7bd4f865183af2
kylajones <kjones29@nd.edu>
2023-11-27 11:17:42 -0500

    ran and updated transformations

--- linfa/tests/test_discr_04_LF_nodiscr_HFData_TP1.py
+++ linfa/tests/test_discr_04_LF_nodiscr_HFData_TP1.py
@@ -23,7 +23,7 @@ def run_test():
     exp.input_size          = 2             # int: Dimensionalty of input (default 2)
     exp.batch_size          = 200           # int: Number of samples generated (default 100)
     exp.true_data_num       = 2             # double: Number of true model evaluted (default 2)
-    exp.n_iter              = 2501         # int: Number of iterations (default 25001)
+    exp.n_iter              = 25001         # int: Number of iterations (default 25001)
     exp.lr                  = 0.001         # float: Learning rate (default 0.003)
     exp.lr_decay            = 0.9999        # float:  Learning rate decay (default 0.9999)
     exp.log_interal         = 10            # int: How often to show loss stat (default 10)
@@ -85,8 +85,7 @@ def run_test():
 
         # Initialize negative log likelihood
         total_nll = torch.zeros((calib_inputs.size(0), 1))
-        
-        # HAD TO MOVE THIS UP BEFORE EVALUATE DISCREPANCY            
+                 
         # Evaluate model response - (num_var x num_batch)
         modelOut = langmuir_model.solve_t(transform.forward(calib_inputs)).t()
 
@@ -107,9 +106,8 @@ def run_test():
         # Evaluate log-likelihood:
         # Loop on the available observations
         for loopA in range(num_obs):
+
             l1 = -0.5 * np.prod(langmuir_model.data.shape) * np.log(2.0 * np.pi)
-            
-            # TODO: generalize to multiple inputs
             l2 = (-0.5 * langmuir_model.data.shape[1] * torch.log(torch.prod(stds))).item()
             l3 = -0.5 * torch.sum(((modelOut + discrepancy.t() - Data[:,loopA].unsqueeze(0)) / stds.t())**2, dim = 1)
             
@@ -144,7 +142,7 @@ def generate_data():
 # Main code
 if __name__ == "__main__":
     
-    # generate_data()
+    generate_data()
     
     run_test()
 
