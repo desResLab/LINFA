e77b1a3f68da3d3b06e2dc6f0ebe2cf0122de9a9
kylajones <kjones29@nd.edu>
2023-12-12 13:36:23 -0500

    changed commands

--- linfa/tests/test_plot_discr.sh
+++ linfa/tests/test_plot_discr.sh
@@ -1,6 +1,6 @@
  # discr_surface
 python3 -m linfa.plot_disc --folder results/ \
-							--name test_lf_with_disc_hf_data_prior_TP15_rep_meas \
+							--name test_lf_with_disc_hf_data_prior_TP1 \
 							--iter 15000 \
 							--mode histograms \
 							--num_points 10 \
@@ -8,9 +8,9 @@ python3 -m linfa.plot_disc --folder results/ \
 
 
 
-python3 -m linfa.plot_disc --folder results/ \
-							--name test_lf_with_disc_hf_data_prior_TP15_rep_meas \
-							--iter 15000 \
-							--mode discr_surface \
-							--num_points 40 \
-							--limfactor 2.0 \
+# python3 -m linfa.plot_disc --folder results/ \
+# 							--name test_lf_with_disc_hf_data_prior_TP15_rep_meas \
+# 							--iter 15000 \
+# 							--mode discr_surface \
+# 							--num_points 40 \
+# 							--limfactor 2.0 \
