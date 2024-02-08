19de378b4aa3e7095d98c8ee5dc81eb0c92086f5
kylajones <kjones29@nd.edu>
2024-02-08 14:25:53 -0500

    updated to plot results from test 19

--- run_plot_res.sh
+++ run_plot_res.sh
@@ -1,9 +1,9 @@
-# python3 linfa/plot_res.py --folder results/ --name test_07_lf_w_disc_TP15_linear --iter 10000 --picformat png
+python3 linfa/plot_res.py --folder results/ --name test_19_lf_w_disc_TP15_rep_meas_dropout --iter 40000 --picformat png
 
-# python3 linfa/plot_disc.py --folder results/ --name test_07_lf_w_disc_TP15_linear --iter 10000 --picformat png --mode histograms --num_points 10 --limfactor 1.0 --saveinterval 1000
-python3 linfa/plot_disc.py --folder results/ --name test_07_lf_w_disc_TP15_linear --iter 10000 --picformat png --mode discr_surface --num_points 10 --limfactor 1.0 --saveinterval 1000
-# python3 linfa/plot_disc.py --folder results/ --name test_07_lf_w_disc_TP15_linear --iter 10000 --picformat png --mode marginal_stats --num_points 10 --limfactor 1.0 --saveinterval 1000
-# python3 linfa/plot_disc.py --folder results/ --name test_07_lf_w_disc_TP15_linear --iter 10000 --picformat png --mode marginal_posterior --num_points 10 --limfactor 1.0 --saveinterval 1000
+python3 linfa/plot_disc.py --folder results/ --name test_19_lf_w_disc_TP15_rep_meas_dropout --iter 40000 --picformat png --mode histograms --num_points 10 --limfactor 1.0 --saveinterval 1000
+python3 linfa/plot_disc.py --folder results/ --name test_19_lf_w_disc_TP15_rep_meas_dropout --iter 40000 --picformat png --mode discr_surface --num_points 10 --limfactor 1.0 --saveinterval 1000
+# python3 linfa/plot_disc.py --folder results/ --name test_19_lf_w_disc_TP15_rep_meas_dropout --iter 50000 --picformat png --mode marginal_stats --num_points 10 --limfactor 1.0 --saveinterval 1000
+python3 linfa/plot_disc.py --folder results/ --name test_19_lf_w_disc_TP15_rep_meas_dropout --iter 40000 --picformat png --mode marginal_posterior --num_points 10 --limfactor 1.0 --saveinterval 1000
 
 
 
