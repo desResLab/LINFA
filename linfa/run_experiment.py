fixed line in likelihood for std dev calculation

linfa/run_experiment.py
@@ -266,15 +266,17 @@ class experiment:
                         else:
                             np.savetxt(self.output_dir + '/' + self.name + '_outputs_' + str(iteration), (self.model.solve_t(self.transform.forward(xkk)) + noise).data.cpu().numpy(), newline="\n")
                     elif(self.surrogate_type == 'discrepancy'):
-                        # TODO confirm the following changes with DS
+                        # TODO confirm the following changes with 
                         ## Define noise when we use NoFAS
                         # stds = torch.abs(self.model.defOut).to(self.device) * self.model.stdRatio
-                        stds = torch.mean(torch.abs(self.model.defOut)).to(self.device) * self.model.stdRatio
+                        params = self.transform.forward(xkk)
+                        data = torch.tensor(self.model.data[:,2:]).to(self.device)
+                        stds = (torch.mean(data).to(self.device) * params[:, 2]).unsqueeze(0)
                         # Noise is rows: number of T,P pairs, columns: number of batches
                         o00 = torch.randn(self.model.data.shape[0], x00.size(0)).to(self.device)
-                        noise = o00*stds.repeat(1,x00.size(0))
-                        # Print lf outputs
-                        model_out = self.model.solve_t(self.transform.forward(xkk))
+                        # 
+                        noise = o00*stds #.repeat(1,x00.size(0))
+                        model_out = self.model.solve_t(params)
                         np.savetxt(self.output_dir + '/' + self.name + '_outputs_lf_' + str(iteration), model_out.data.cpu().numpy(), newline="\n")                    
                         # LF model, plus dicrepancy, plus noise
                         if(self.surrogate is None):

linfa/tests/test_no_disc_TP15_error_estimation.py
@@ -4,6 +4,8 @@ from linfa.discrepancy import Discrepancy
 import torch
 import random
 import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
 
 # Import rcr model
 from linfa.models.discrepancy_models import PhysChem_error
@@ -26,7 +28,7 @@ def run_test():
     exp.batch_size          = 200           # int: Number of samples generated (default 100)
     exp.true_data_num       = 2             # double: Number of true model evaluted (default 2)
     exp.n_iter              = 10000         # int: Number of iterations (default 25001)
-    exp.lr                  = 0.0001        # float: Learning rate (default 0.003)
+    exp.lr                  = 0.001 #00075       # float: Learning rate (default 0.003)
     exp.lr_decay            = 0.9999        # float:  Learning rate decay (default 0.9999)
     exp.log_interval        = 1             # int: How often to show loss stat (default 10)
 
@@ -52,7 +54,6 @@ def run_test():
     exp.lr_scheduler = 'ExponentialLR'
 
     exp.device = torch.device('cuda:0' if torch.cuda.is_available() and not exp.no_cuda else 'cpu')
-
 
     # Define transformation
     trsf_info = [['tanh', -40.0, 30.0, 500.0, 1500.0],
@@ -98,9 +99,6 @@ def run_test():
            
         # Evaluate model response - (num_var x num_batch)
         modelOut = model.solve_t(transform.forward(calib_inputs)).t()
-        
-        # Evaluate default model output to compute standard deviation
-        def_out = model.solve_t(model.defParams)
 
         # Evaluate discrepancy
         if (surrogate is None):
@@ -109,102 +107,73 @@ def run_test():
             # (num_var)
             discrepancy = surrogate.forward(model.var_in)
         
-        # Get the absolute values of the standard deviation (num_var)
-        p0Const, eConst, sigma_e = torch.chunk(phys_inputs, chunks = 3, dim = 1)
-        std_dev = sigma_e.flatten() * torch.mean(torch.abs(def_out))
-
+        # Get the calibration parameters
+        p0Const, eConst, std_dev_ratio = torch.chunk(phys_inputs, chunks = 3, dim = 1)
+        
         # Get data - (num_var x num_obs)
         Data = torch.tensor(model.data[:,2:]).to(exp.device)
-        num_obs = Data.size(1)
+        num_reapeat_obs = Data.size(1)
+
+        # Convert standard deviation ratio to standard deviation
+        std_dev = std_dev_ratio.flatten() * torch.mean(Data)
         
         # Evaluate log-likelihood:
         # Loop on the available observations
-        for loopA in range(num_obs):
+        for loopA in range(num_reapeat_obs):
             
             # -n / 2 * log ( 2 pi ) 
             l1 = -0.5 * np.prod(model.data.shape[1]) * np.log(2.0 * np.pi)
 
-            # Ask about this likelihood
-            # - n / 2 * log (sigma^2)
-            l2 = (-0.5 * model.data.shape[1] * torch.log(std_dev ** 2))
+            # - n / 2 * log (sigma)
+            l2 = -0.5 * model.data.shape[1] * torch.log(std_dev)
             
             # - 1 / (2 * sigma^2) sum_{i = 1} ^ N (eta_i + disc_i - y_i)^2 
             l3 = -0.5 / (std_dev ** 2) * torch.sum((modelOut + discrepancy.t() - Data[:,loopA].unsqueeze(0))**2, dim = 1)
 
             negLL = -(l1 + l2 + l3) # sum contributions
             res = -negLL.reshape(calib_inputs.size(0), 1) # reshape
-        
+
             # Accumulate
             total_nll += res
 
-            fin_res = total_nll/num_obs + adjust
-
-            # print('total nll', (total_nll/num_obs).mean().item())
-            # print('adjust', adjust.mean().item())
-            # print('total', fin_res.mean().item())
-            # exit()
+            fin_res = total_nll/num_reapeat_obs + adjust
                 
         # Return log-likelihood
         return fin_res
 
     # Assign log density model
     exp.model_logdensity = lambda x: log_density(x, exp.model, exp.surrogate, exp.transform)
-
+   
     # Define log prior
     def log_prior(calib_inputs, transform):
-        
+        # TODO: figure out why calib_inputs are coming in on physical basus
+
         # Compute transformation log Jacobian
         adjust = transform.compute_log_jacob_func(calib_inputs)
         
         # Compute the calibration inputs in the physical domain
         phys_inputs = transform.forward(calib_inputs)
-
-        # Define prior moments for first two variables
-        pr_avg = torch.tensor([[1E3, -21E3]])
-        pr_std = torch.tensor([[1E2, 500]])
-
-        # Eval log prior
-        # -n / 2 * log ( 2 pi ) 
-        l1 = -0.5 * calib_inputs.size(1) * np.log(2.0 * np.pi)     
-
-        # - n / 2 * log (sigma^2)       
-        l2 = (-0.5 * torch.log(torch.prod(pr_std))).item()
         
-        # - 1 / (2 * sigma^2) sum_{i = 1} ^ N (eta_i + disc_i - y_i)^2 
-        l3 = -0.5 * torch.sum(((phys_inputs[:,:1] - pr_avg)/pr_std)**2, dim = 1).unsqueeze(1)
-        # Add gaussian log prior for first two parameters
-        gauss_prior_res = l1 + l2 + l3
+        # Gaussian prior on physically meaningful inputs
+        mean = np.array([1.0E3,-21.0E3])
+        cov = np.array([[100.0**2, 0.0],
+                        [0.0,   500.0**2]])
+        gauss_prior_res = torch.from_numpy(stats.multivariate_normal.logpdf(phys_inputs[:,:2].detach().numpy(), mean = mean, cov = cov))
         
-        if True:
-             # Add beta prior for third parameter
-            sigma_prior = torch.distributions.beta.Beta(torch.tensor([3/16]), torch.tensor([57/16]))
-            prior_res = sigma_prior.log_prob(phys_inputs[:,2])
-        else:
-            # Add uniform prior
-            a = torch.tensor([0.0])
-            b = torch.tensor([0.15])
-            sigma_prior = torch.distributions.uniform.Uniform(low = a, high = b)
-            prior_res = torch.zeros(phys_inputs[:,2].size(0))
-            for loopA, siggy in enumerate(phys_inputs[:,2]):
-                if a <= siggy <= b:
-                    prior_res[loopA] = sigma_prior.log_prob(siggy)
-                else:
-                    # Large negative number
-                    prior_res[loopA] = 500
+        # Beta prior on noise
+        std_dev_ratio = torch.distributions.beta.Beta(torch.tensor([2.0]), torch.tensor([19.0]))
         
-        res = gauss_prior_res + prior_res +  adjust
+        # Wrong prior being applied to phys_inputs
+        prior_res = std_dev_ratio.log_prob(phys_inputs[:,2])
 
-        # print('Prior on std. dev. ratio:', prior_res.mean().item())
-        # print('Gaussian prior on physical params:', gauss_prior_res.mean().item())
-        # print('Adjustment:', adjust.mean().item())
-        # print('Total:', res.mean().item())
+        # xvalues = torch.linspace(0, 1, 100)
+        # plt.plot(xvalues.numpy(), torch.exp(std_dev_ratio.log_prob(xvalues)).numpy())
+        # plt.hist(phys_inputs[:,2].detach().numpy(), density = True)
+        # plt.xlim(0,0.6)
+        # plt.show()
         # exit()
 
-
-        # Log prior is constributing the most to the loss function
-        # Loss function is insensitive to values of sigma
-        # Add transformation
-        
+        res = gauss_prior_res + prior_res +  adjust
         return res
 
     exp.model_logprior = lambda x: log_prior(x, exp.transform)
@@ -228,15 +197,6 @@ def generate_data(use_true_model=False,num_observations=50):
 # Main code
 if __name__ == "__main__":
 
-    # Add beta prior for third parameter
-    # test = torch.distributions.beta.Beta(torch.tensor([1.0]), torch.tensor([3.0]))
-    # samples = test.sample_n(50)
-    # print(samples)
-    # exit()
-
     generate_data(use_true_model = False, num_observations = 1)
 
     run_test()
-
-
-

linfa/tests/test_no_disc_TP15_error_estimation_uniform_prior.py
@@ -4,6 +4,7 @@ from linfa.discrepancy import Discrepancy
 import torch
 import random
 import numpy as np
+from scipy import stats
 
 # Import rcr model
 from linfa.models.discrepancy_models import PhysChem_error
@@ -162,35 +163,30 @@ def run_test():
         
         # Compute the calibration inputs in the physical domain
         phys_inputs = transform.forward(calib_inputs)
-    
-        # Define prior moments for first two variables
-        pr_avg = torch.tensor([[1E3, -21E3]])
-        pr_std = torch.tensor([[1E2, 5E2]])
 
-        # Eval log prior
-        # -n / 2 * log ( 2 pi ) 
-        l1 = -0.5 * calib_inputs.size(1) * np.log(2.0 * np.pi)     
+        if False:
+            # Define prior moments for first two variables
+            pr_avg = torch.tensor([[1E3, -21E3]])
+            pr_std = torch.tensor([[1E2, 5E2]])
 
-        # - n / 2 * log det(Sigma)       
-        l2 = -0.5 * torch.log(torch.prod(pr_std**2))
-        
-        # - 1 / (2 * sigma^2) sum_{i = 1} ^ N (eta_i + disc_i - y_i)^2 
-        l3 = -0.5 * torch.sum(((phys_inputs[:,:1] - pr_avg)/pr_std)**2, dim = 1).unsqueeze(1)
-        # Add gaussian log prior for first two parameters
-        gauss_prior_res = l1 + l2 + l3
-        print(gauss_prior_res)
-        exit()
-        ## TODO: debug with scipy
-        # import numpy as np
-        # from scipy.stats import multivariate_normal
-        # mean = np.array([1000.0,-21000.0])
-        # cov = np.array([[100.0**2,0],
-        #                 [0.0,500.0**2]])
-        # rv = multivariate_normal(mean=mean, cov=cov)
-        # sample = np.array([1200.0,-20000.0])
-        # print(rv.logpdf(sample))
-        # print('Completed!')
-        
+            # Eval log prior
+            # -n / 2 * log ( 2 pi ) 
+            l1 = -0.5 * calib_inputs.size(1) * np.log(2.0 * np.pi)     
+
+            # - n / 2 * log det(Sigma)       
+            l2 = -0.5 * torch.log(torch.prod(pr_std**2))
+            
+            # - 1 / (2 * sigma^2) sum_{i = 1} ^ N (eta_i + disc_i - y_i)^2 
+            l3 = -0.5 * torch.sum(((phys_inputs[:,:1] - pr_avg)/pr_std)**2, dim = 1).unsqueeze(1)
+            # Add gaussian log prior for first two parameters
+            gauss_prior_res = l1 + l2 + l3
+        else:
+            mean = np.array([1000.0,-21000.0])
+            cov = np.array([[100.0**2,0],
+                            [0.0,500.0**2]])
+            print(phys_inputs[:,:2].detach().numpy())
+            gauss_prior_res = stats.multivariate_normal.logpdf(phys_inputs[:,:2].detach().numpy(), mean = mean, cov = cov)
+            
         # Add uniform prior
         sigma_prior = torch.distributions.uniform.Uniform(low = torch.tensor([0.0]), high = torch.tensor([1.0]))
         prior_res = torch.zeros(phys_inputs[:,2].size(0))
@@ -200,9 +196,8 @@ def run_test():
             else:
                 # Large negative number
                 prior_res[loopA] = -500
-                print(sigma)
-                exit()
-        res = gauss_prior_res + prior_res.unsqueeze(1) +  adjust
+                
+        res = torch.from_numpy(gauss_prior_res) + prior_res.unsqueeze(1) +  adjust
 
         # print('Prior on std. dev. ratio:', prior_res.size())
         # print('Gaussian prior on physical params:', gauss_prior_res.size())